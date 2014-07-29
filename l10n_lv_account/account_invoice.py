90922cc5b89dac678e1f2f1651f864c412b62a5c
Santa <santa@its1.lv>
2014-07-29 16:11:21 +0300

    account upgrade.

--- l10n_lv_account/account_invoice.py
+++ l10n_lv_account/account_invoice.py
@@ -22,14 +22,8 @@
 #
 ##############################################################################
 
-import time
-from lxml import etree
-import openerp.exceptions
-
-from openerp import netsvc
-from openerp import pooler
-from openerp.osv import fields, osv, orm
-from openerp.tools.translate import _
+from openerp import api
+from openerp.osv import osv, fields
 
 class account_invoice(osv.osv):
     _inherit = "account.invoice"
@@ -39,82 +33,22 @@ class account_invoice(osv.osv):
         'address_invoice_id': fields.many2one('res.partner.address', 'Invoice Address', readonly=True, states={'draft':[('readonly',False)]})
     }
 
-    def onchange_partner_id(self, cr, uid, ids, type, partner_id,\
-            date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):
-        partner_payment_term = False
-        acc_id = False
-        bank_id = False
-        fiscal_position = False
+    @api.multi
+    def onchange_partner_id(self, type, partner_id, date_invoice=False,
+            payment_term=False, partner_bank_id=False, company_id=False):
+        res = super(account_invoice, self).onchange_partner_id(type, partner_id, date_invoice=date_invoice,
+            payment_term=payment_term, partner_bank_id=partner_bank_id, company_id=company_id)
         address_invoice_id = False
         address_contact_id = False
-
-        opt = [('uid', str(uid))]
-        if partner_id:
-
-            opt.insert(0, ('id', partner_id))
-            p = self.pool.get('res.partner').browse(cr, uid, partner_id)
-            if company_id:
-                if (p.property_account_receivable.company_id and (p.property_account_receivable.company_id.id != company_id)) and (p.property_account_payable.company_id and (p.property_account_payable.company_id.id != company_id)):
-                    property_obj = self.pool.get('ir.property')
-                    rec_pro_id = property_obj.search(cr,uid,[('name','=','property_account_receivable'),('res_id','=','res.partner,'+str(partner_id)+''),('company_id','=',company_id)])
-                    pay_pro_id = property_obj.search(cr,uid,[('name','=','property_account_payable'),('res_id','=','res.partner,'+str(partner_id)+''),('company_id','=',company_id)])
-                    if not rec_pro_id:
-                        rec_pro_id = property_obj.search(cr,uid,[('name','=','property_account_receivable'),('company_id','=',company_id)])
-                    if not pay_pro_id:
-                        pay_pro_id = property_obj.search(cr,uid,[('name','=','property_account_payable'),('company_id','=',company_id)])
-                    rec_line_data = property_obj.read(cr,uid,rec_pro_id,['name','value_reference','res_id'])
-                    pay_line_data = property_obj.read(cr,uid,pay_pro_id,['name','value_reference','res_id'])
-                    rec_res_id = rec_line_data and rec_line_data[0].get('value_reference',False) and int(rec_line_data[0]['value_reference'].split(',')[1]) or False
-                    pay_res_id = pay_line_data and pay_line_data[0].get('value_reference',False) and int(pay_line_data[0]['value_reference'].split(',')[1]) or False
-                    if not rec_res_id and not pay_res_id:
-                        raise osv.except_osv(_('Configuration Error!'),
-                            _('Cannot find a chart of accounts for this company, you should create one.'))
-                    account_obj = self.pool.get('account.account')
-                    rec_obj_acc = account_obj.browse(cr, uid, [rec_res_id])
-                    pay_obj_acc = account_obj.browse(cr, uid, [pay_res_id])
-                    p.property_account_receivable = rec_obj_acc[0]
-                    p.property_account_payable = pay_obj_acc[0]
-
-            if type in ('out_invoice', 'out_refund'):
-                acc_id = p.property_account_receivable.id
-                partner_payment_term = p.property_payment_term and p.property_payment_term.id or False
-            else:
-                acc_id = p.property_account_payable.id
-                partner_payment_term = p.property_supplier_payment_term and p.property_supplier_payment_term.id or False
-            fiscal_position = p.property_account_position and p.property_account_position.id or False
-            if p.bank_ids:
-                bank_id = p.bank_ids[0].id
-            if p.address_id:
-                for addr in p.address_id:
-                    address_invoice_id = addr.id
-                    address_contact_id = addr.id
-                    break
-
-        result = {'value': {
-            'account_id': acc_id,
-            'payment_term': partner_payment_term,
-            'fiscal_position': fiscal_position,
-            'address_invoice_id': address_invoice_id,
-            'address_contact_id': address_contact_id
-            }
-        }
-
-        if type in ('in_invoice', 'in_refund'):
-            result['value']['partner_bank_id'] = bank_id
-
-        if payment_term != partner_payment_term:
-            if partner_payment_term:
-                to_update = self.onchange_payment_term_date_invoice(
-                    cr, uid, ids, partner_payment_term, date_invoice)
-                result['value'].update(to_update['value'])
-            else:
-                result['value']['date_due'] = False
-
-        if partner_bank_id != bank_id:
-            to_update = self.onchange_partner_bank(cr, uid, ids, bank_id)
-            result['value'].update(to_update['value'])
-        return result
-
-account_invoice()
+        p = self.env['res.partner'].browse(partner_id)
+        if p.address_id:
+            for addr in p.address_id:
+                address_invoice_id = addr.id
+                address_contact_id = addr.id
+                break
+        if not res.get('value',False):
+            res = {'value': {}}
+        res['value'].update({'address_contact_id': address_contact_id, 'address_invoice_id': address_invoice_id})
+        return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
