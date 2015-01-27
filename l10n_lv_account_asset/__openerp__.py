ff9da3a7a65ff089c8a3a391cc0126827670d208
Santa <santa@its1.lv>
2015-01-27 13:03:46 +0200

    Asset confirmation Report.

--- l10n_lv_account_asset/__openerp__.py
+++ l10n_lv_account_asset/__openerp__.py
@@ -54,6 +54,7 @@ My Company filter for Assets.
     'update_xml': [
         'account_asset_view.xml',
         'views/asset_card_report.xml',
+        'views/asset_confirmation_report.xml',
         'account_asset_report.xml',
         'wizard/account_asset_set_close_date_view.xml',
         'wizard/report_account_asset_turnover_view.xml',
