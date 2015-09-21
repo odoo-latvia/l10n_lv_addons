fd6c27fa4b67edfa19b0d9faf25cf3f7a380da92
Santa <santa@its1.lv>
2015-09-21 17:13:10 +0300

    Fidavista: account.payment instead of account.voucher.

--- l10n_lv_fidavista/wizard/account_fidavista_export.py
+++ l10n_lv_fidavista/wizard/account_fidavista_export.py
@@ -29,16 +29,7 @@ import base64
 import openerp.addons.decimal_precision as dp
 import time
 from datetime import datetime
-
-class account_fidavista_export_payments(osv.osv_memory):
-    _name = 'account.fidavista.export.payments'
-
-    _columns = {
-        'wizard_id': fields.many2one('account.fidavista.export', 'Wizard'),
-        'payment_number': fields.char('Payment Number', size=32),
-        'partner_id': fields.many2one('res.partner', 'Partner', required=True),
-        'bank_account_id': fields.many2one('res.partner.bank', 'Bank Account', required=True)
-    }
+import string
 
 class account_fidavista_export(osv.osv_memory):
     _name = 'account.fidavista.export'
@@ -46,65 +37,31 @@ class account_fidavista_export(osv.osv_memory):
 
     _columns = {
         'name': fields.char('File Name', size=32),
-        'period_id': fields.many2one('account.period','Period', required=True),
         'file_save': fields.binary('Save File', filters='*.xml', readonly=True),
-        'partner_id': fields.many2one('res.partner', 'Related Partner'),
-        'bank_account_id': fields.many2one('res.partner.bank', 'Bank Account', required=True),
-        'payments_ids': fields.one2many('account.fidavista.export.payments', 'wizard_id', 'Payment Data')
+        'payment_order_id': fields.many2one('payment.order', 'Payment Order', required=False),
     }
-
-    def _get_period(self, cr, uid, context=None):
-        if context is None:
-            context = {}
-        period_obj = self.pool.get('account.period')
-        date = time.strftime('%Y-%m-%d')
-        period_id = period_obj.search(cr, uid, [('date_start','<=',date),('date_stop','>=',date)], context=context)
-        if period_id:
-            return period_id[0]
-        return False
-
-    def _get_partner(self, cr, uid, context=None):
-        obj_user = self.pool.get('res.users')
-        partner_id = obj_user.browse(cr, uid, uid, context=context).company_id.partner_id.id or False
-        return partner_id
 
     _defaults = {
-        'name': 'FiDAViSta_export.xml',
-        'period_id': _get_period,
-        'partner_id': _get_partner
+        'name': 'FiDAViSta_export.xml'
     }
 
-    def onchange_period_id(self, cr, uid, ids, period_id, bank_account, context=None):
+    def onchange_payment_order_id(self, cr, uid, ids, payment_order_id, context=None):
         if context is None:
             context = {}
-        payment_vals = []
-        bank_acc_obj = self.pool.get('res.partner.bank')
-        payment_obj = self.pool.get('account.voucher')
-        payment_ids = payment_obj.search(cr, uid, [('period_id','=',period_id)], context=context)
-        for payment in payment_obj.browse(cr, uid, payment_ids, context=context):
-            bank_account_id = False
-            for bank_acc in payment.partner_id.bank_ids:
-                if bank_account:
-                    bank_acc_c = bank_acc_obj.browse(cr, uid, bank_account, context=context)
-                    if bank_acc.bank.id == bank_acc_c.bank.id:
-                        bank_account_id = bank_acc.id
-                        break
-                    else:
-                        bank_account_id = bank_acc.id
-                else:
-                    bank_account_id = bank_acc.id
-            payment_vals.append({'payment_number': payment.number, 'partner_id': payment.partner_id.id, 'bank_account_id': bank_account_id})
-        return {'value': {'payments_ids': payment_vals}}
+        res = {}
+        if payment_order_id:
+            payment = self.pool.get('payment.order').browse(cr, uid, payment_order_id, context=context)
+            res = {'value': {'name': '%s.xml' % payment.reference}}
+        return res
 
     def create_xml(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-
-        data_exp = self.browse(cr, uid, ids[0])
-        bank_acc_obj = self.pool.get('res.partner.bank')
-        payment_obj = self.pool.get('account.voucher')
-        payment_ids = payment_obj.search(cr, uid, [('period_id','=',data_exp.period_id.id)], context=context)
-
+        data_exp = self.browse(cr, uid, ids[0], context=context)
+        if context.get('payment_order_id',False):
+            payment_order = self.pool.get('payment.order').browse(cr, uid, context['payment_order_id'], context=context)
+        else:
+            payment_order = data_exp.payment_order_id
         data_of_file = """<?xml version="1.0" encoding="UTF-8" ?>
 <FIDAVISTA xmlns="http://bankasoc.lv/fidavista/fidavista0101.xsd">"""
         data_of_file += "\n    <Header>"
@@ -112,102 +69,98 @@ class account_fidavista_export(osv.osv_memory):
         company = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id
         data_of_file += ("\n        <From>" + company.name + "</From>")
         data_of_file += "\n    </Header>"
-
-        for payment in payment_obj.browse(cr, uid, payment_ids, context=context):
+        pay_acc = payment_order.mode.bank_id.acc_number.replace(' ', '').upper()
+        pay_legal = False
+        if hasattr(payment_order.mode.bank_id.partner_id, 'company_registry'):
+            pay_legal = payment_order.mode.bank_id.partner_id.company_registry
+        if (not pay_legal) and hasattr(payment_order.mode.bank_id.partner_id, 'vat') and payment_order.mode.bank_id.partner_id.vat:
+            pay_legal = ((payment_order.mode.bank_id.partner_id.vat).replace(' ', '').upper())[2:]
+        if (not pay_legal) and hasattr(payment_order.mode.bank_id.partner_id, 'identification_id') and payment_order.mode.bank_id.partner_id.is_company == False:
+            pay_legal = payment_order.mode.bank_id.partner_id.identification_id
+        data_obj = self.pool.get('ir.model.data')
+        for payment in payment_order.line_ids:
             data_of_file += "\n    <Payment>"
-            if payment.reference:
-                data_of_file += ("\n        <ExtId>" + payment.reference + "</ExtId>")
-            data_of_file += ("\n        <DocNo>" + payment.number + "</DocNo>")
-            data_of_file += ("\n        <RegDate>" + payment.date + "</RegDate>")
+            ext_ids = data_obj.search(cr, uid, [('model','=','payment.line'), ('res_id','=',payment.id)], context=context)
+            if ext_ids:
+                extid = data_obj.browse(cr, uid, ext_ids[0], context=context).complete_name
+                data_of_file += ("\n        <ExtId>" + extid + "</ExtId>")
+            data_of_file += ("\n        <DocNo>" + payment.name + "</DocNo>")
+            if payment.date:
+                data_of_file += ("\n        <RegDate>" + payment.date + "</RegDate>")
             data_of_file += ("\n        <TaxPmtFlg>" + "N" + "</TaxPmtFlg>")
-            data_of_file += ("\n        <Ccy>" + payment.payment_rate_currency_id.name + "</Ccy>")
-            note = payment.narration
-            if not note:
-                note = payment.partner_id.name + " " + payment.number
-            data_of_file += ("\n        <PmtInfo>" + note + "</PmtInfo>")
-            if company.company_registry:
-                data_of_file += ("\n        <PayLegalId>" + company.company_registry + "</PayLegalId>")
-            data_of_file += ("\n        <PayAccNo>" + (data_exp.bank_account_id.acc_number).replace(" ","") + "</PayAccNo>")
-            if payment.currency_id:
-                data_of_file += ("\n        <DebitCcy>" + payment.currency_id.name + "</DebitCcy>")
+            data_of_file += ("\n        <Ccy>" + payment.currency.name + "</Ccy>")
+            data_of_file += ("\n        <PmtInfo>" + payment.communication + "</PmtInfo>")
+            if pay_legal:
+                data_of_file += ("\n        <PayLegalId>" + pay_legal + "</PayLegalId>")
+            data_of_file += ("\n        <PayAccNo>" + pay_acc + "</PayAccNo>")
+            data_of_file += ("\n        <DebitCcy>" + payment.company_currency.name + "</DebitCcy>")
             data_of_file += ("\n        <BenSet>")
-            data_of_file += ("\n            <BenExtId>" + str(payment.partner_id.id) + "</BenExtId>")
+            ben_ext_ids = data_obj.search(cr, uid, [('model','=','res.partner'), ('res_id','=',payment.partner_id.id)], context=context)
+            if ben_ext_ids:
+                benextid = data_obj.browse(cr, uid, ben_ext_ids[0], context=context).complete_name
+                data_of_file += ("\n            <BenExtId>" + benextid + "</BenExtId>")
             data_of_file += ("\n            <Priority>" + "N" + "</Priority>")
-            comm = "SHA"
-            if (payment.currency_id) and (payment.currency_id.id != payment.payment_rate_currency_id.id):
-                comm = "OUR"
-            if (not payment.currency_id) and (company.currency_id.id != payment.payment_rate_currency_id.id):
-                comm = "OUR"
-            data_of_file += ("\n            <Comm>" + comm + "</Comm>")
-            data_of_file += ("\n            <Amt>") + str(payment.amount) + ("</Amt>")
-            bank_acc_no = False
-            bank_acc = False
-            for wiz_payment in data_exp.payments_ids:
-                if payment.number == wiz_payment.payment_number:
-                    bank_acc = wiz_payment.bank_account_id
-                    bank_acc_no = (bank_acc.acc_number).replace(" ","")
-                    break
-            data_of_file += ("\n            <BenAccNo>" + bank_acc_no + "</BenAccNo>")
+            data_of_file += ("\n            <Comm>" + "OUR" + "</Comm>")
+            data_of_file += ("\n            <Amt>") + str(payment.amount_currency) + ("</Amt>")
+            if not payment.bank_id:
+                raise osv.except_osv(_('Not enought Data Error !'), _('Destination Bank account not defined for payment %s!' % payment.name))
+            data_of_file += ("\n            <BenAccNo>" + (payment.bank_id.acc_number).replace(' ','').upper() + "</BenAccNo>")
             flg = "N"
-            if bank_acc.state == 'iban':
+            if payment.bank_id.state == 'iban':
                 flg = "Y"
             data_of_file += ("\n            <BenAccIbanFlg>" + flg + "</BenAccIbanFlg>")
             data_of_file += ("\n            <BenName>" + payment.partner_id.name + "</BenName>")
-            vat = hasattr(payment.partner_id,'company_registry') and payment.partner_id.company_registry or False
-            if (not vat) and (payment.partner_id.vat):
-                vat = ((payment.partner_id.vat).replace(" ","").upper())[2:]
-            if vat:
-                data_of_file += ("\n            <BenLegalId>" + vat + "</BenLegalId>")
-            address = ""
-            city = payment.partner_id.city
-            if city:
-                address += city
-            street = payment.partner_id.street
-            if street:
-                if payment.partner_id.street2:
-                    street = street + " " + payment.partner_id.street2
-                if city:
-                    address += " "
-                address += street
-            zip = payment.partner_id.zip
-            if zip:
-                if city or street:
-                    address += ", "
-                address += zip
-            if address != "":
-                data_of_file += ("\n            <BenAddress>" + address + "</BenAddress>")
+            ben_legal = hasattr(payment.partner_id,'company_registry') and payment.partner_id.company_registry or False
+            if (not ben_legal) and hasattr(payment.partner_id, 'vat') and (payment.partner_id.vat):
+                ben_legal = ((payment.partner_id.vat).replace(" ","").upper())[2:]
+            if (not ben_legal) and hasattr(payment.partner_id, 'identification_id') and payment.partner_id.is_company == False:
+                ben_legal = payment.partner_id.identification_id
+            if ben_legal:
+                data_of_file += ("\n            <BenLegalId>" + ben_legal + "</BenLegalId>")
+            address_list = []
+            if payment.partner_id.street:
+                address_list.append(payment.partner_id.street)
+            if payment.partner_id.street2:
+                address_list.append(payment.partner_id.street2)
+            if payment.partner_id.city:
+                address_list.append(payment.partner_id.city)
+            if payment.partner_id.state_id:
+                address_list.append(payment.partner_id.state_id.name)
+            if payment.partner_id.zip:
+                address_list.append(payment.partner_id.zip)
+            if payment.partner_id.country_id:
+                address_list.append(payment.partner_id.country_id.name)
+            if address_list:
+                data_of_file += ("\n            <BenAddress>" + ", ".join(address_list) + "</BenAddress>")
             country_code = False
             if payment.partner_id.country_id:
                 country_code = payment.partner_id.country_id.code
-                if (not country_code) and payment.partner_id.vat:
+                if (not country_code) and hasattr(payment.partner_id, 'vat') and payment.partner_id.vat:
                     country_code = (payment.partner_id.vat).upper()[:2]
             if country_code:
                 data_of_file += ("\n            <BenCountry>" + country_code + "</BenCountry>")
             if not country_code:
                 raise osv.except_osv(_('Insufficient data!'), _('No Country or VAT defined for Partner "%s", but the Country Code is a mandatory tag. Please define either a Country or a VAT to get the Country Code!') % (payment.partner_id.name))
-            bank_name = bank_acc.bank_name or (bank_acc.bank and bank_acc.bank.name) or False
+            bank_name = payment.bank_id.bank and payment.bank_id.bank.name or payment.bank_id.bank_name
             if bank_name:
                 data_of_file += ("\n            <BBName>" + bank_name + "</BBName>")
-            if bank_acc.bank:
-                bank_address = ""
-                b_city = bank_acc.bank.city
-                if b_city:
-                    bank_address += b_city
-                b_street = bank_acc.bank.street
-                if b_street:
-                    if bank_acc.bank.street2:
-                        b_street = b_street + " " + bank_acc.bank.street2
-                    if b_city:
-                        bank_address += " "
-                    bank_address += b_street
-                b_zip = bank_acc.bank.zip
-                if b_zip:
-                    if b_city or b_street:
-                        bank_address += ", "
-                    bank_address += b_zip
-                if bank_address != "":
-                    data_of_file += ("\n            <BBAddress>" + bank_address + "</BBAddress>")
-            swift = bank_acc.bank_bic or (bank_acc.bank and bank_acc.bank.bic) or False
+            if payment.bank_id.bank:
+                bank_address_list = []
+                if payment.bank_id.bank.street:
+                    bank_address_list.append(payment.bank_id.bank.street)
+                if payment.bank_id.bank.street2:
+                    bank_address_list.append(payment.bank_id.bank.street2)
+                if payment.bank_id.bank.city:
+                    bank_address_list.append(payment.bank_id.bank.city)
+                if payment.bank_id.bank.state:
+                    bank_address_list.append(payment.bank_id.bank.state.name)
+                if payment.bank_id.bank.zip:
+                    bank_address_list.append(payment.bank_id.bank.zip)
+                if payment.bank_id.bank.country:
+                    bank_address_list.append(payment.bank_id.bank.country.name)
+                if bank_address_list:
+                    data_of_file += ("\n            <BBAddress>" + ", ".join(bank_address_list) + "</BBAddress>")
+            swift = payment.bank_id.bank and payment.bank_id.bank.bic or payment.bank_id.bank_bic
             if swift:
                 data_of_file += ("\n            <BBSwift>" + swift + "</BBSwift>")
             data_of_file += ("\n        </BenSet>")
@@ -216,7 +169,10 @@ class account_fidavista_export(osv.osv_memory):
         data_of_file += "\n</FIDAVISTA>"
 
         data_of_file_real = base64.encodestring(data_of_file.encode('utf8'))
-        self.write(cr, uid, ids, {'file_save': data_of_file_real, 'name': data_exp.name}, context=context)
+        self.write(cr, uid, ids, {
+            'file_save': data_of_file_real,
+            'name': data_exp.name
+        }, context=context)
 
         return {
             'type': 'ir.actions.act_window',
@@ -227,6 +183,5 @@ class account_fidavista_export(osv.osv_memory):
             'views': [(False,'form')],
             'target': 'new',
         }
-
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
