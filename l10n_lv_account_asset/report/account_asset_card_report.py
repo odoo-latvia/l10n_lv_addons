a2a676c4989da51b8bcb238cf1209bd7c4cac6db
santa <santa@its1.lv>
2016-09-01 17:57:16 +0300

    Asset card report: usage years and months, amortization values.

--- l10n_lv_account_asset/report/account_asset_card_report.py
+++ l10n_lv_account_asset/report/account_asset_card_report.py
@@ -26,6 +26,7 @@ import time
 from datetime import datetime
 from openerp.report import report_sxw
 from openerp.osv import osv
+from dateutil.relativedelta import relativedelta
 
 class account_asset_card_report(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
@@ -34,8 +35,81 @@ class account_asset_card_report(report_sxw.rml_parse):
             'time': time,
             'cr':cr,
             'uid': uid,
+            'get_time_depr_data': self.get_time_depr_data
         })
-        
+
+    def get_time_depr_data(self, o):
+        method_number = o.method_number
+        asset_obj = self.pool.get('account.asset.asset')
+        cur_obj = self.pool.get('res.currency')
+
+        base_date = o.start_date or (o.state == 'open' and o.confirmation_date) or o.purchase_date
+        date_val = datetime.strptime(base_date, '%Y-%m-%d')
+        if o.next_month:
+            month = date_val.month + 1
+            year = date_val.year
+            if date_val.month == 12:
+                month = 1
+                year += 1
+            date_val = datetime(year, month, 1)
+        if (not o.next_month) and (not o.prorata):
+            date_val = datetime(date_val.year, date_val.month, 1)
+        day = date_val.day
+        month = date_val.month
+        year = date_val.year
+        total_days = (year % 4) and 365 or 366
+
+        undone_dotation_number = asset_obj._compute_board_undone_dotation_nb(self.cr, self.uid, o, date_val, total_days, context=self.localcontext)
+        if o.method_time == 'end':
+            method_number = undone_dotation_number
+
+        if o.method_time == 'number' and o.accumulated_depreciation != 0.0:
+            amount_to_depr = residual_amount = o.purchase_value - o.accumulated_depreciation
+            base_amount = 0.0
+            for x in range(0, undone_dotation_number):
+                i = x + 1
+                amount = asset_obj._compute_board_amount(self.cr, self.uid, o, i, residual_amount, amount_to_depr, undone_dotation_number, [], total_days, date_val, context=self.localcontext)
+                residual_amount -= amount
+                date_val = (datetime(year, month, day) + relativedelta(months=+o.method_period))
+                day = date_val.day
+                month = date_val.month
+                year = date_val.year
+                base_amount = amount
+                break
+            if base_amount != 0.0:
+                print '-------------'
+                print base_amount
+                if o.method == 'linear':
+                    method_number += int(o.accumulated_depreciation / base_amount)
+                if o.method == 'degressive':
+                    test_amount = 0.0
+                    residual_amount = base_amount
+                    i = 0
+                    while (test_amount < o.accumulated_depreciation):
+                        test_amount += residual_amount / (o.method_progress_factor * 2)
+                        residual_amount += test_amount
+                        i += 1
+                        if i == 1:
+                            residual_amount -= base_amount
+                    method_number = i
+            print '-----------------------'
+            print method_number
+        usage_years = (o.method_period * method_number) / 12.0
+        usage_months = o.method_period * method_number
+        amort_month_val = (o.method_period != 0 and method_number != 0) and (o.purchase_value/(o.method_period*method_number)) or 0.0
+        amort_month_perc = (o.purchase_value != 0.0 and o.method_period != 0 and method_number != 0) and (((o.purchase_value/(o.method_period*method_number))*100)/o.purchase_value) or 0.0
+        amort_val = (usage_years != 0) and (o.purchase_value/usage_years) or 0.0
+        amort_percent = (o.purchase_value != 0.0 and usage_years != 0.0) and (((o.purchase_value/usage_years)*100.0)/o.purchase_value) or 0.0
+        return {
+            'service_date': o.start_date or (o.state == 'open' and o.confirmation_date) or o.purchase_date,
+            'usage_years': usage_years,
+            'usage_months': usage_months,
+            'amort_month_val': amort_month_val,
+            'amort_month_perc': amort_month_perc,
+            'amort_val': amort_val,
+            'amort_percent': amort_percent
+        }
+
 class ac_report(osv.AbstractModel):
     _name = 'report.l10n_lv_account_asset.asset_card_report'
     _inherit = 'report.abstract_report'
