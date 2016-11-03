f6d030674946db627fc8b4f47cd63a8fce8542b6
Karlis <karlisdreizis@gmail.com>
2016-11-03 15:01:04 +0200

    [ADD] initial latvian personal no. check

---
+++ l10n_lv_partner_data_validate/pk.py
@@ -0,0 +1,17 @@
+def validate(pk):
+    """
+    Validate a latvian personal identification number
+    or a company registration numnber.
+    Accepts a string with or withouth a '-'.
+    e.g. 000000-00000
+         00000000000
+    """
+    pk = pk.strip()
+    if (not pk.isdigit() or ('-' in pk and len(pk) != 12) 
+            or ('-' not in pk and len(pk) != 11)):
+        return False
+    pk = list(map(int, pk.replace('-', '')))
+    calc = (1 * pk[0]+6* pk[1]+3* pk[2]+7* pk[3]+9* pk[4]+10* pk[5]+5* 
+            pk[6]+8* pk[7]+4* pk[8]+2* pk[9])
+    checksum = (1101- calc)%11;
+    return  checksum ==  pk[10];
