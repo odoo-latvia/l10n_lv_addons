da7346ae2f3bdc1a78311b0af8701f044efc8894
santa <santa@its1.lv>
2016-12-16 17:47:59 +0200

    l10n_lv: default taxes; l10n_lv_check_company_registry: syntax fix.

--- l10n_lv/models/account.py
+++ l10n_lv/models/account.py
@@ -82,14 +82,14 @@ class AccountConfigSettings(models.TransientModel):
         res = super(AccountConfigSettings, self).onchange_chart_template_id()
         lv_chart_template = self.env.ref('l10n_lv.l10n_lv_chart_template')
         if self.chart_template_id and lv_chart_template and self.chart_template_id.id == lv_chart_template.id:
-#            lv_sale_tax = self.env.ref('l10n_lv.lv_tax_template_PVN-SR')
-#            lv_purchase_tax = self.env.ref('l10n_lv.lv_tax_template_Pr-SR')
-#            if lv_sale_tax:
-#                self.sale_tax_id = lv_sale_tax.id
-#                self.default_sale_tax_id = lv_sale_tax.id
-#            if lv_purchase_tax:
-#                self.purchase_tax_id = lv_purchase_tax.id
-#                self.default_purchase_tax_id = lv_purchase_tax.id
+            lv_sale_tax = self.env.ref('l10n_lv.lv_tax_template_PVN-SR')
+            lv_purchase_tax = self.env.ref('l10n_lv.lv_tax_template_Pr-SR')
+            if lv_sale_tax:
+                self.sale_tax_id = lv_sale_tax.id
+                self.default_sale_tax_id = lv_sale_tax.id
+            if lv_purchase_tax:
+                self.purchase_tax_id = lv_purchase_tax.id
+                self.default_purchase_tax_id = lv_purchase_tax.id
             self.sale_tax_rate = 21.0
             self.purchase_tax_rate = 21.0
         return res
