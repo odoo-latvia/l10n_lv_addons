cc3d307a41cdde2ab3aea765c0d7f4daa2f86b3e
Karlis <karlisdreizis@gmail.com>
2017-07-05 09:52:13 +0300

    [ADD] module mk_date_format

---
+++ l10n_lv_mk_date_format/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Part of Odoo.
+#    Copyright (C) 2016 ITS-1 (<http://www.its1.lv/>)
+#                       E-mail: <info@its1.lv>
+#                       Address: <Vienibas gatve 109 LV-1058 Riga Latvia>
+#                       Phone: +371 67289467
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'LV MK Date Format',
+    'version': '1.0',
+    'description': """
+    Replace odoo's default date format 
+    %Y-%m-%d
+    with mk's
+    %d-%m-%Y
+    """,
+    'author': 'ITS-1',
+    'website': 'http://www.its1.lv/',
+    'category': 'Hidden',
+    'depends': ['base'],
+    'data': [
+        'data.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
