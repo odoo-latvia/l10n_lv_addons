7d6fe3c56b867d22266a4a75cd915bc981418b0b
Karlis <karlisdreizis@gmail.com>
2017-08-03 12:17:23 +0300

    [IMP] verify on input

--- l10n_lv_partner_data_validate/static/src/js/field_validate.js
+++ l10n_lv_partner_data_validate/static/src/js/field_validate.js
@@ -9,14 +9,10 @@ var _t = core._t;
 var Partner = new Model('res.partner');
 var Country = new Model('res.country');
 
-// TODO: make parametarized, not dependent on specific fields in the form
-// aka. make it a widget type
-// TODO: parametarize country field
 var FieldRegistry = formWidget.FieldChar.extend({
     init: function (field_manager, node) {
         this._super(field_manager, node);
-        this.events.keyup = this.proxy(this.validate);
-        this.events.blur = this.proxy(this.validate_blur);
+        this.events.input = this.proxy(this.validate);
         this.show_error = true;
     },
     start: function() {
@@ -38,23 +34,14 @@ var FieldRegistry = formWidget.FieldChar.extend({
     get_pk: function() {
         return this.parse_value(this.$input.val(), '').replace('-', '');
     },
-    validate_blur: function() {
-        var value = this.get_pk();
-
-        if (value.length < 11 && value.length != 0) {
-            this.$input.addClass('o_form_invalid');
-            this.do_warn(_t('Validation error'), _t('Number too short'));
-        }
-    },
     validate: function() {
         var value = this.get_pk();
 
-        if (value.length > 11) {
+        if (value.length != 11) {
             this.$input.addClass('o_form_invalid');
-            this.show_warning(_t('Validation error'), _t('Number too long'));
             this.set('valid', false);
         }
-        else if (value.length == 11) {
+        else {
 
             Partner.call('frontend_check', [value]).then((function (valid) {
                 if (!valid) {
@@ -66,7 +53,6 @@ var FieldRegistry = formWidget.FieldChar.extend({
                     this.set('valid', true);
                 }
 
-                console.log('is it valid')
             }).bind(this))
 
         }
