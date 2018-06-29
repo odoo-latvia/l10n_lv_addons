c2e103e39d4d9e2b114a2057dd9bdf308e168a27
karlis <karlis@karlis-pc.ozolsgrupa.ozols.lv>
2018-06-29 16:11:30 +0300

    [ADD] l10n_lv_stock and update account & invocie

--- l10n_lv_account/models/sales_order.py
+++ l10n_lv_account/models/sales_order.py
@@ -14,3 +14,10 @@ class SaleOrder(models.AbstractModel):
         if num2words:
             return lambda n: num2words(n, lang=self.env.lang, to='currency')
         return None
+
+    @api.multi
+    def invoice_print(self):
+        """ Print the invoice and mark it as sent, so that we can see more
+            easily the next step of the workflow
+        """
+        return self.env.ref('l10n_lv_account.action_report_docket_invoice').report_action(self)
