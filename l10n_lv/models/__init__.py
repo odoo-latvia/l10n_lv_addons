efca80e8853c31927e62d12b025f7b111013647c
Janis Viesis <you@example.com>
2019-03-12 18:35:04 +0200

    Some xml files set as comments

--- l10n_lv/models/__init__.py
+++ l10n_lv/models/__init__.py
@@ -22,6 +22,6 @@
 #
 ##############################################################################
 
-from . import account
+#from . import account
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
