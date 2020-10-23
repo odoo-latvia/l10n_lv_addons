1416e7547d6dd5bf08d9dc819e18918da6151e36
ieva <ieva@dev06.ozols.lv>
2020-10-23 12:45:58 +0300

    l10n: modified data records

--- l10n_lv/__manifest__.py
+++ l10n_lv/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Part of Odoo.
-#    Copyright (C) 2019 Allegro IT (<http://www.allegro.lv/>)
+#    Copyright (C) 2020 Allegro IT (<http://www.allegro.lv/>)
 #                       E-mail: <info@allegro.lv>
 #                       Address: <Vienibas gatve 109 LV-1058 Riga Latvia>
 #                       Phone: +371 67289467
@@ -24,7 +24,7 @@
 
 {
     'name': 'Latvia - Accounting',
-    'version': '14.0',
+    'version': '1.1',
     'category': 'Localization',
     'summary': 'Accounting configuration for Latvia',
     'description': """
@@ -37,14 +37,14 @@ Adds Chart of Accounts, Chart of Taxes, Fiscal Positions, Banks and Partner Titl
         'account'
     ],
     'data': [
-        'data/account_chart_template_data.xml',
         'data/res_company_data.xml',
         'data/account_group_data.xml',
         'data/account_account_type_data.xml',
         'data/account_account_tag_data.xml',
+        'data/account_chart_template_data.xml',
         'data/account_account_template_data.xml',
-        #'data/account_tax_template_data.xml',
-        #'data/account_fiscal_position_template_data.xml',
+        'data/account_tax_template_data.xml',
+        'data/account_fiscal_position_template_data.xml',
         'data/res_partner_title_data.xml',
         'data/res.bank.csv',
         'views/res_partner_view.xml'
