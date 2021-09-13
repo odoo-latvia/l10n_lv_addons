4aed642bb3d6b3ca529138377d33d51c94851e5c
Santa Ašmane <santa.asmane@allegro.lv>
2021-09-13 14:41:50 +0300

    l10n_lv_partner_data_firmaslv: firmaslv widget finished in test mode.

---
+++ l10n_lv_partner_data_firmaslv/__manifest__.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Part of Odoo.
+#    Copyright (C) 2021 Ozols Grupa (<http://www.ozols.lv/>)
+#                       E-mail: <info@ozols.lv>
+#                       Address: <Vienibas gatve 109 LV-1058 Riga Latvia>
+#                       Phone: +371 67289211
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Partner data form firmas.lv',
+    'version': '1.1',
+    'category': 'Localization',
+    'description': """
+Partner data loading from firmas.lv
+=====================================
+Adds an option to load partner's data from firmas.lv database.
+""",
+    'author': 'Ozols Grupa',
+    'website': 'http://www.ozols.lv',
+    'images': [],
+    'depends': [
+        'l10n_lv_partner_data'
+    ],
+    'data': [
+        'views/firmaslv_assets.xml',
+        'views/res_partner_views.xml'
+    ],
+    'qweb': [
+        'static/src/xml/firmaslv.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
